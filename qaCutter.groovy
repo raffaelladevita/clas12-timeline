b2e820543149927d62143c414169c06b9a090595
christopher dilks <christopher.j.dilks@gmail.com>
2019-11-18 17:43:13 -0500

    still modularizing

--- qaCutter.groovy
+++ qaCutter.groovy
@@ -53,16 +53,19 @@ def inList = inTdir.getCompositeObjectList(inTdir)
     }
     ...
   }
-// also define 'cutTree', which follows the same structure as 'ratioTree', but
-// with the [ list of N/F values ] replaced with [ cut boundaries (map) ]
+// - also define 'cutTree', which follows the same structure as 'ratioTree', but
+//   with the [ list of N/F values ] replaced with [ cut boundaries (map) ]
+// - also define 'epochPlotTree', for plotting N/F within each epoch
 */
 def ratioTree = [:]
 def cutTree = [:]
+def epochPlotTree = [:]
 
 // initialize sector branches
 sectors.each{ 
   ratioTree.put(sec(it),[:])
   cutTree.put(sec(it),[:])
+  epochPlotTree.put(sec(it),[:])
 }
 
 // loop over 'grA' graphs (of N/F vs. filenum), filling ratioTree leaves
@@ -78,6 +81,9 @@ inList.each { obj ->
     if(ratioTree[sector][epoch]==null) {
       ratioTree[sector].put(epoch,[])
       cutTree[sector].put(epoch,[:])
+      epochPlotTree[sector].put(epoch,new GraphErrors("epoch_${epoch}_sector_${sector}"))
+      epochPlotTree[sector][epoch].setTitle(
+        "N/F vs. file index -- epoch ${epoch}, sector ${sector}")
     }
 
     // append N/F values to the list associated to this (sector,epoch)
@@ -124,7 +130,8 @@ jPrint("cuts.json",cutTree) // output cutTree to JSON
 
 
 // define output hipo file
-def outHipo = new TDirectory()
+def outHipoRuns = new TDirectory()
+def outHipoEpochs = new TDirectory()
 
 // define timeline graphs
 def TL = sectors.collect { s ->
@@ -160,7 +167,12 @@ def splitGraph = { g ->
 def copyPoint = { g1,g2,i ->
   g2.addPoint(g1.getDataX(i),g1.getDataY(i),g1.getDataEX(i),g1.getDataEY(i))
 }
-def writeHipo = { outList -> outList.each{ outHipo.addDataSet(it) } }
+def addEpochPlotPoint = { plot,val ->
+  def n = plot.getDataSize(0) + 1
+  plot.addPoint(n,val,0,0)
+}
+
+def writeHipo = { hipo,outList -> outList.each{ hipo.addDataSet(it) } }
   
 // loop over grA graphs, apply the QA cuts, and fill 'good' and 'bad' graphs
 def ratio
@@ -184,6 +196,7 @@ inList.each { obj ->
     // loop through points in grA and fill good and bad graphs
     grA.getDataSize(0).times { i -> 
       ratio = grA.getDataY(i)
+      addEpochPlotPoint(epochPlotTree[sector][epoch],ratio)
       if(ratio > cutTree[sector][epoch]['cutLo'] &&
          ratio < cutTree[sector][epoch]['cutHi']) {
         copyPoint(grA,grA_good,i)
@@ -199,15 +212,26 @@ inList.each { obj ->
     }
 
     // write graphs to hipo file
-    //if(!(outHipo.exists("/${runnum}"))) outHipo.mkdir("/${runnum}")
-    outHipo.mkdir("/${runnum}")
-    outHipo.cd("/${runnum}")
-    writeHipo([
-      grA_good,grA_bad,
-      grN_good,grN_bad,
-      grF_good,grF_bad,
-      grT_good,grT_bad
-    ])
+    outHipoRuns.mkdir("/${runnum}")
+    outHipoRuns.cd("/${runnum}")
+    writeHipo(
+      outHipoRuns,
+      [
+        grA_good,grA_bad,
+        grN_good,grN_bad,
+        grF_good,grF_bad,
+        grT_good,grT_bad
+      ]
+    )
+
+    /* aqui
+    outHipoEpochs.mkdir("/${sector}")
+    outHipoEpochs.cd("/${sector}")
+    writeHipo(
+      outHipoEpochs,
+      epochPlotTree[sector].collect{...}
+    )
+    */
 
     // add QA passing fraction to timeline graph
     nGood = grA_good.getDataSize(0)
@@ -221,12 +245,12 @@ inList.each { obj ->
 }
 
 // write timelines to output hipo file
-outHipo.mkdir("/timelines")
-outHipo.cd("/timelines")
-TL.each { outHipo.addDataSet(it) }
+outHipoRuns.mkdir("/timelines")
+outHipoRuns.cd("/timelines")
+TL.each { outHipoRuns.addDataSet(it) }
 
 // write hipo file to disk
 def outHipoN = "timeline_vsRun.hipo"
 File outHipoFile = new File(outHipoN)
 if(outHipoFile.exists()) outHipoFile.delete()
-outHipo.writeFile(outHipoN)
+outHipoRuns.writeFile(outHipoN)

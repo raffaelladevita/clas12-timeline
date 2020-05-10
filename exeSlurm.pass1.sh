575864160cdd717e8fca649b1675789e0fc15733
Christopher Dilks <dilks@ifarm1901.jlab.org>
2020-05-09 22:48:50 -0400

    increase job time limit

--- exeSlurm.pass1.sh
+++ exeSlurm.pass1.sh
@@ -36,7 +36,7 @@ app "#SBATCH --account=clas12"
 app "#SBATCH --partition=production"
 
 app "#SBATCH --mem-per-cpu=6000"
-app "#SBATCH --time=4:00:00"
+app "#SBATCH --time=6:00:00"
 
 app "#SBATCH --array=1-$(cat $joblist | wc -l)"
 app "#SBATCH --ntasks=1"

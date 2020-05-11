a16b87d395a2726947f73069ac6e5d4186c100c6
christopher dilks <christopher.j.dilks@gmail.com>
2020-05-11 17:36:44 -0400

    fix pretty printer at the end of meld.groovy

--- QA/meld/meld.groovy
+++ QA/meld/meld.groovy
@@ -1,14 +1,5 @@
 // combine a new qaTree.json file with an old one
-// - this is used if you've made a major update to the QA, and you
-//   want to "meld" the new results with old results
-// - you need to control which defect bits you want to overwrite:
-//   - some bits you will prefer to use the old qaTree.json version
-//   - other bits you may prefer to use the new ones
-// - this script is "one-time-use", so you need to read it carefully before
-//   running it
-// - it assumes the file names are `qaTree.json.old` and `qaTree.json.new`
-// - the ouptut will be `qaTree.json.melded`
-// - be sure to backup any qaTree.json files in case something goes wrong
+// - see README.md
 
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
@@ -111,7 +102,4 @@ qaTreeNew.each{ runnum, fileTree ->
 
 // output melded qaTree.json
 new File("qaTree.json.melded").write(JsonOutput.toJson(qaTreeMelded))
-['new','old','melded'].each{
-  "groovy ../parseQaTree.groovy qaTree.json.$it".execute()
-}
-"./prettyPrint.sh".execute()
+['new','old','melded'].each{"./prettyPrint.sh $it".execute()}
--- QA/meld/prettyPrint.sh
+++ QA/meld/prettyPrint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# pretty print a json file (called by meld.sh)
+# (called by meld.sh)
 groovy ../../jprint.groovy qaTree.json.$1 > qaTree.json.${1}.pprint
+groovy ../parseQaTree.groovy qaTree.json.$1

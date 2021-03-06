a62711cb8c37e0a7ed5094640b1d835840e7f9de
Sangbaek <sangbaek@mit.edu>
2021-03-06 14:34:35 -0500

    collect executables from submodules

---
+++ bin/detectors.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export binDir=`dirname $0`
+export runDir="$binDir/../run_based_monitoring/bin/"
+
+./$runDir/run.sh
---
+++ bin/qa.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export binDir=`dirname $0`
+export clasqaDir="$binDir/../clasqa/calib/"
+
+./clasqaDir/qa.sh

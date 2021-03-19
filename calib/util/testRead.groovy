40b560e25a2a57317e3a5b210386c43d3533a23c
christopher dilks <christopher.j.dilks@gmail.com>
2021-03-18 22:54:03 -0400

    remove calib directory (it has been moved to new repo clas12-timeline)

--- calib/util/testRead.groovy
+++
@@ -1,5 +0,0 @@
-import org.jlab.groot.data.TDirectory
-def hipoFile = "/u/group/clas/www/clas12mon/html/hipo/rga/pass0/v2.2.29/dc/dc_residuals_sec_mean.hipo"
-if(args.length>=1) hipoFile = args[0]
-def inTdir = new TDirectory()
-inTdir.readFile(hipoFile)

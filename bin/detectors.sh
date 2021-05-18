f0c2dbf39f584b977d668f681b6abd7ca727c919
Sangbaek <sangbaek@mit.edu>
2021-05-18 17:24:45 -0400

    minimal work through run_based_monitoring release

--- bin/detectors.sh
+++ bin/detectors.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-export binDir=`dirname $0`
-export runDir="$binDir/../run_based_monitoring/bin/"
-
-./$runDir/run.sh
+if [ $1 = "setup" ]; then
+	echo "downloading detector timeline"
+	
+	mkdir ../detectors
+	cd ../detectors
+	wget https://github.com/Sangbaek/run_based_monitoring/releases/download/v1.0/run.sh
+	wget https://github.com/Sangbaek/run_based_monitoring/releases/download/v1.0/timelineMon-1.0-SNAPSHOT.jar
+	chmod +x run.sh
+	mkdir bin
+	mkdir target
+	mv run.sh bin/
+	mv timelineMon-1.0-SNAPSHOT.jar target/
+else
+	echo "running detector timeline"
+	cd ../detectors
+	./bin/run.sh $1
+fi

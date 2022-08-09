de168ffbbd67cfb9d1f261da2cc0b5a237b60750
christopher dilks <christopher.j.dilks@gmail.com>
2022-08-08 23:02:55 -0400

    modified:   calib-qa/util/testRead.groovy (to test for HIPO file reading issue)

--- calib-qa/util/testRead.groovy
+++ calib-qa/util/testRead.groovy
@@ -1,5 +1,6 @@
 import org.jlab.groot.data.TDirectory
-def hipoFile = "/u/group/clas/www/clas12mon/html/hipo/rga/pass0/v2.2.29/dc/dc_residuals_sec_mean.hipo"
-if(args.length>=1) hipoFile = args[0]
+def hipoFile = "/u/group/clas/www/clas12mon/html/hipo/rga/pass0/v2.2.52/rf/rftime_electron_FD_mean.hipo"
 def inTdir = new TDirectory()
+// File inTdirFile
+// inTdirFile = new File(hipoFile)
 inTdir.readFile(hipoFile)

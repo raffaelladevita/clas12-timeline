ddc167548d62d768daa66113593a7d16347ec166
esteejus <justin.estee@gmail.com>
2022-10-19 09:42:20 -0400

    Adding distance of closest approach timelines for CVT vertex to monitor the CVT-DC alignment

---
+++ detectors/src/main/java/org/jlab/clas/timeline/timeline/cvt/cvt_d0_max_pos.groovy
@@ -0,0 +1,40 @@
+package org.jlab.clas.timeline.timeline.cvt
+import java.util.concurrent.ConcurrentHashMap
+import org.jlab.groot.data.TDirectory
+import org.jlab.groot.data.GraphErrors
+
+class cvt_d0_max_pos {
+
+def data = new ConcurrentHashMap()
+
+def processDirectory(dir, run) {
+  def h1 = dir.getObject('/cvt/H_CVT_absd0_pos')
+
+  data[run] = [run:run, hlist:h1]
+}
+
+
+
+def close() {
+
+  TDirectory out = new TDirectory()
+
+  def grtl = new GraphErrors('cvt_d0_pos')
+  grtl.setTitle("Std dev. for positives")
+  grtl.setTitleY("Std dev. for positives (cm)")
+  grtl.setTitleX("run number")
+
+  data.sort{it.key}.each{run,it->
+    out.mkdir('/'+it.run)
+    out.cd('/'+it.run)
+    out.addDataSet(it.hlist)
+    def h1 = it.hlist
+    grtl.addPoint(it.run, h1.getXaxis().getBinCenter(h1.getMaximumBin()), 0, 0)
+  }
+
+  out.mkdir('/timelines')
+  out.cd('/timelines')
+  grtl.each{ out.addDataSet(it) }
+  out.writeFile('cvt_d0_max_pos.hipo')
+}
+}

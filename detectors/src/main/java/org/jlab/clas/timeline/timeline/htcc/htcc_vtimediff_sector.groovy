65492fff265ffc822661beaea5c98c89ca7dce34
Andrey <kenjo@jlab.org>
2023-04-05 11:24:22 -0400

    added vtimediff averaged over sector and ring, fix fits

--- detectors/src/main/java/org/jlab/clas/timeline/timeline/htcc/htcc_vtimediff_sector.groovy
+++ detectors/src/main/java/org/jlab/clas/timeline/timeline/htcc/htcc_vtimediff_sector.groovy
@@ -1,19 +1,21 @@
 package org.jlab.clas.timeline.timeline.htcc
 import org.jlab.groot.data.H1F
 import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.ConcurrentLinkedQueue
 import org.jlab.groot.data.TDirectory
 import org.jlab.groot.data.GraphErrors
 import org.jlab.clas.timeline.fitter.HTCCFitter
 
-class htcc_vtimediff {
+class htcc_vtimediff_sector {
 
   def data = new ConcurrentHashMap()
 
   def processDirectory(dir, run) {
 
-    [(0..<6), (0..<4), (0..<2)].combinations().each{s,r,side->
-      def h0 = dir.getObject(String.format("/HTCC/H_HTCC_vtime_s%d_r%d_side%d",s+1,r+1,side+1))
+    (0..<6).each{sec->
+      def hlist = [(0..<4),(0..<2)].combinations().collect{ring,side->dir.getObject("/HTCC/H_HTCC_vtime_s${sec+1}_r${ring+1}_side${side+1}")}
+      def h0 = hlist.head()
+      hlist.tail().each{h0.add(it)}
+
       def maxbin = h0.getMaximumBin()
 
       def h1 = new H1F(h0.getName(), h0.getTitle(), 200, h0.getDataX(maxbin)-100.5*h0.getDataEX(0), h0.getDataX(maxbin)+99.5*h0.getDataEX(0))
@@ -21,7 +23,7 @@ class htcc_vtimediff {
 
       def f1 = HTCCFitter.timeIndPMT(h1)
 
-      def ttl = "sector ${s+1} ring ${r+1} side ${side+1}"
+      def ttl = "sector ${sec+1}"
       data.computeIfAbsent(ttl, {[]}).add([run:run, h1:h1, f1:f1, mean:f1.getParameter(1), sigma:f1.getParameter(2).abs()])
     }
 
@@ -54,7 +56,7 @@ class htcc_vtimediff {
         out.cd('/timelines')
         out.addDataSet(grtl)
       }
-      out.writeFile("htcc_vtimediff_${name}.hipo")
+      out.writeFile("htcc_vtimediff_sector_${name}.hipo")
     }
   }
 

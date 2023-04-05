08291f34c0976f3ee5f835072d29328b55e4ee15
Andrey <kenjo@jlab.org>
2023-04-05 11:25:33 -0400

    improve fitting

--- detectors/src/main/java/org/jlab/clas/timeline/fitter/HTCCFitter.groovy
+++ detectors/src/main/java/org/jlab/clas/timeline/fitter/HTCCFitter.groovy
@@ -12,27 +12,22 @@ import org.jlab.groot.math.F1D
 
 
 class HTCCFitter{
-	static F1D timeIndPMT(H1F h1) {
-        int nBinsTime = 4000;
-        double lowTime = -500;
-        double highTime = 500;
-        def f1 = new F1D("fit:"+h1.getName(), "[amp]*gaus(x,[mean],[sigma])", -500, 500);
-        // f1.setParameter(0, 500);
-        // f1.setParameter(1, -0.0);
-        // f1.setParameter(2, 0.7);
-        f1.setLineColor(2);
-        f1.setLineWidth(2);
-        f1.setOptStat(1101);
-        double maxV = h1.getMaximumBin();
-        maxV = lowTime + (maxV + 0.5) * (highTime - lowTime) / nBinsTime;
-        f1.setParameter(0, h1.getMax());
-        f1.setParameter(1, maxV);
-        f1.setParameter(2, 0.6);
-        f1.setRange(maxV - 1, maxV + 1.3);
-        DataFitter.fit(f1, h1, "");
+  static F1D timeIndPMT(H1F h1) {
+    double maxV = h1.getDataX(h1.getMaximumBin());
+    def f1 = new F1D("fit:"+h1.getName(), "[amp]*gaus(x,[mean],[sigma])", maxV-2, maxV+3);
 
-		return f1
- 	}
+    f1.setLineColor(2);
+    f1.setLineWidth(2);
+    f1.setOptStat(1101);
+
+    f1.setParameter(0, h1.getMax());
+    f1.setParameter(1, maxV);
+    f1.setParameter(2, 0.6);
+
+    DataFitter.fit(f1, h1, "");
+
+    return f1
+  }
 
  	static F1D timeAllFit(H1F h1){
         F1D f1 = new F1D("fit:"+h1.getName(), "[amp]*gaus(x,[mean],[sigma])", -1, 1);

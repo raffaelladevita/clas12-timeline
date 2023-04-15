bfe82833896c872eddb68904b508890ed0ebe6b1
Andrey <kenjo@jlab.org>
2023-04-14 22:44:08 -0400

    update nphe timelines

--- detectors/src/main/java/org/jlab/clas/timeline/timeline/htcc/htcc_nphe_ring_sector.groovy
+++ detectors/src/main/java/org/jlab/clas/timeline/timeline/htcc/htcc_nphe_ring_sector.groovy
@@ -8,18 +8,20 @@ class htcc_nphe_ring_sector {
 def data = new ConcurrentHashMap()
 
 def processDirectory(dir, run) {
-  def histlist =   (0..<6).collect{s->
-    (0..<4).collect{r->
-      def h1 = dir.getObject(String.format('/HTCC/H_HTCC_nphe_s%d_r%d_side1',s+1,r+1)) //left
-      def h2 = dir.getObject(String.format('/HTCC/H_HTCC_nphe_s%d_r%d_side2',s+1,r+1)) //right
+  (1..6).each{sec->
+    (1..4).each{ring->
+      def h1 = dir.getObject("/HTCC/H_HTCC_nphe_s${sec}_r${ring}_side1") //left
+      def h2 = dir.getObject("/HTCC/H_HTCC_nphe_s${sec}_r${ring}_side2") //right
       h1.add(h2)
-      h1.setName("sec"+(s+1) +"ring"+(r+1))
+
+      def name = "sec $sec ring $ring"
+      h1.setName(name)
       h1.setTitle("HTCC Number of Photoelectrons")
       h1.setTitleX("HTCC Number of Photoelectrons")
-      return h1
+
+      data.computeIfAbsent(name, {[]}).add([run: run, h1: h1])
     }
   }
-  data[run] = [run:run, hlist:histlist]
 }
 
 
@@ -28,24 +30,21 @@ def close() {
 
   TDirectory out = new TDirectory()
   out.mkdir('/timelines')
-  (0..<6).each{ sec->
-    (0..<4).each{ ring ->
-      def grtl = new GraphErrors('sec'+(sec+1)+' ring'+(ring+1))
-      grtl.setTitle("Average HTCC Number of Photoelectrons per sector per ring")
-      grtl.setTitleY("Average HTCC Number of Photoelectrons per sector per ring")
-      grtl.setTitleX("run number")
 
-      data.sort{it.key}.each{run,it->
-        if (sec==0 && ring ==0){
-          out.mkdir('/'+it.run)
-        }
-        out.cd('/'+it.run)
-        out.addDataSet(it.hlist[sec][ring])
-        grtl.addPoint(it.run, it.hlist[sec][ring].getMean(), 0, 0)
-      }
-      out.cd('/timelines')
-      out.addDataSet(grtl)
+  data.each{name,runs->
+    def grtl = new GraphErrors(name)
+    grtl.setTitle("Average HTCC Number of Photoelectrons per sector per ring")
+    grtl.setTitleY("Average HTCC Number of Photoelectrons per sector per ring")
+    grtl.setTitleX("run number")
+
+    runs.sort{it.run}.each{
+      out.mkdir('/'+it.run)
+      out.cd('/'+it.run)
+      out.addDataSet(it.h1)
+      grtl.addPoint(it.run, it.h1.getMean(), 0, 0)
     }
+    out.cd('/timelines')
+    out.addDataSet(grtl)
   }
 
   out.writeFile('htcc_nphe_sec_ring.hipo')
--- detectors/src/main/java/org/jlab/clas/timeline/timeline/htcc/htcc_nphe_sector.groovy
+++ detectors/src/main/java/org/jlab/clas/timeline/timeline/htcc/htcc_nphe_sector.groovy
@@ -8,11 +8,18 @@ class htcc_nphe_sector {
 def data = new ConcurrentHashMap()
 
 def processDirectory(dir, run) {
-  def histlist =   (0..<6).collect{
-    def h1 = dir.getObject('/trig/H_trig_S'+(it+1)+'_HTCC_n')
-    return h1
+  (1..6).each{sec->
+
+    def hlist = [(1..4), [1,2]].combinations().collect{ring,side -> dir.getObject("/HTCC/H_HTCC_nphe_s${sec}_r${ring}_side$side")}
+    def h1 = hlist.head()
+    hlist.tail().each{h1.add(it)}
+
+    h1.setName("sec ${sec}")
+    h1.setTitle("HTCC Number of Photoelectrons")
+    h1.setTitleX("HTCC Number of Photoelectrons")
+
+    data.computeIfAbsent(sec, {[]}).add([run:run, h1:h1])
   }
-  data[run] = [run:run, hlist:histlist]
 }
 
 
@@ -21,19 +28,18 @@ def close() {
 
   TDirectory out = new TDirectory()
   out.mkdir('/timelines')
-  (0..<6).each{ sec->
-    def grtl = new GraphErrors('sec'+(sec+1))
+
+  data.each{sec,runs->
+    def grtl = new GraphErrors("sec $sec")
     grtl.setTitle("Average HTCC Number of Photoelectrons per sector")
     grtl.setTitleY("Average HTCC Number of Photoelectrons per sector")
     grtl.setTitleX("run number")
 
-    data.sort{it.key}.each{run,it->
-      if (sec==0){
-        out.mkdir('/'+it.run)
-      }
+    runs.sort{it.run}.each{
+      out.mkdir('/'+it.run)
       out.cd('/'+it.run)
-      out.addDataSet(it.hlist[sec])
-      grtl.addPoint(it.run, it.hlist[sec].getMean(), 0, 0)
+      out.addDataSet(it.h1)
+      grtl.addPoint(it.run, it.h1.getMean(), 0, 0)
     }
     out.cd('/timelines')
     out.addDataSet(grtl)

e0afcb7c9a1caddb19335b5c9159ae616a7d0fbe
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-07-27 18:32:21 -0400

    refactor: remove duplicated `Tools.groovy` (#63)

--- qa-physics/env.sh
+++ qa-physics/env.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-export CLASQA=$(dirname $(realpath $0))
+if [ -z "${BASH_SOURCE[0]}" ]; then
+  export CLASQA=$(dirname $(realpath $0))
+else
+  export CLASQA=$(dirname $(realpath ${BASH_SOURCE[0]}))
+fi
+
 export CLASQAWWW="/u/group/clas/www/clas12mon/html/hipo"
 JYPATH="${JYPATH}:${CLASQA}"
 export JYPATH=$(echo $JYPATH | sed 's/^://')
 export CLASQA_JAVA_OPTS="-Djava.awt.headless=true"
 
-echo "Environment:"
-env | grep -w COATJAVA
-env | grep -w CLASQA
-env | grep -w CLASQAWWW
-env | grep -w JYPATH
-env | grep -w CLASQA_JAVA_OPTS
+echo """
+--- Environment ---
+COATJAVA         = $COATJAVA
+CLASQA           = $CLASQA
+CLASQAWWW        = $CLASQAWWW
+JYPATH           = $JYPATH
+CLASQA_JAVA_OPTS = $CLASQA_JAVA_OPTS
+-------------------
+"""

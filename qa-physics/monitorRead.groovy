5609ed2bc832abd1bf389bf049545048199b2b80
Christopher Dilks <c-dilks@users.noreply.github.com>
2023-10-24 21:12:58 -0400

    fix: do not correct for wrong helicity (#130)

--- qa-physics/monitorRead.groovy
+++ qa-physics/monitorRead.groovy
@@ -95,18 +95,6 @@ else if(runnum>=15019 && runnum<=15884) RG="RGM"
 else System.err.println "WARNING: unknown run group; using default run-group-dependent settings (see monitorRead.groovy)"
 println "rungroup = $RG"
 
-// helFlip: if true, REC::Event.helicity has opposite sign from reality
-def helFlip = false
-if(RG=="RGA") helFlip = true
-else if(RG=="RGB") {
-  helFlip = true
-  if(runnum>=11093 && runnum<=11283) helFlip = false // fall, 10.4 GeV period only
-  else if(runnum>=11323 && runnum<=11571) helFlip = false // winter
-};
-else if(RG=="RGK") helFlip = false
-else if(RG=="RGF") helFlip = true
-else if(RG=="RGM") helFlip = true
-
 // beam energy
 // - hard-coded; could instead get from RCDB, but sometimes it is incorrect
 def EBEAM = 10.6041 // RGA default
@@ -720,7 +708,6 @@ inHipoList.each { inHipoFile ->
     // get helicity and fill helicity distribution
     if(event.hasBank("REC::Event")) helicity = eventBank.getByte('helicity',0)
     else helicity = 0 // (undefined)
-    if(helFlip) helicity *= -1 // flip helicity (needed for RGA pass1)
     switch(helicity) {
       case 1:  helStr='hp'; helDefined=true; break
       case -1: helStr='hm'; helDefined=true; break

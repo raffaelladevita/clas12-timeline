193756ad055cd6c51a68bd5672a7f7fb9141b581
izzy <izzy@jlab.org>
2024-01-03 16:57:15 -0500

    add normalization factor timelines (#83)

--- detectors/src/main/java/org/jlab/clas/timeline/timeline/htcc/htcc_nphe_ring_sector.groovy
+++ detectors/src/main/java/org/jlab/clas/timeline/timeline/htcc/htcc_nphe_ring_sector.groovy
@@ -5,48 +5,65 @@ import org.jlab.groot.data.GraphErrors
 
 class htcc_nphe_ring_sector {
 
-def data = new ConcurrentHashMap()
+  def data = new ConcurrentHashMap()
 
-def processDirectory(dir, run) {
-  (1..6).each{sec->
-    (1..4).each{ring->
-      def h1 = dir.getObject("/HTCC/H_HTCC_nphe_s${sec}_r${ring}_side1") //left
-      def h2 = dir.getObject("/HTCC/H_HTCC_nphe_s${sec}_r${ring}_side2") //right
-      h1.add(h2)
+  def processDirectory(dir, run) {
+    // histogram with the nphe for all channels in a single run
+    def h_npheAll = dir.getObject('/HTCC/npheAll')
+    def averageNphe = h_npheAll.getMean() 
+    // Now we can add run, histogram, and correction factor to data
+    (1..6).each{sec->
+      (1..4).each{ring->
+        def h1 = dir.getObject("/HTCC/H_HTCC_nphe_s${sec}_r${ring}_side1") //left
+        def h2 = dir.getObject("/HTCC/H_HTCC_nphe_s${sec}_r${ring}_side2") //right
+        h1.add(h2)
 
-      def name = "sec $sec ring $ring"
-      h1.setName(name)
-      h1.setTitle("HTCC Number of Photoelectrons")
-      h1.setTitleX("HTCC Number of Photoelectrons")
-
-      data.computeIfAbsent(name, {[]}).add([run: run, h1: h1])
+        def name = "sec $sec ring $ring"
+        // Calculate the correction factor
+        def correctionFactor = averageNphe > 0 ? h1.getMean() / averageNphe : 0
+        // Store the histogram and correction factor
+        data.computeIfAbsent(name, {[]}).add([run: run, h1: h1, correctionFactor: correctionFactor]) 
+      }
     }
   }
-}
 
+  def close() {
+    ['npheMean', 'normFactor'].each { plotType ->
+      TDirectory out = new TDirectory()
+      out.mkdir('/timelines')
 
+      data.each { name, runs ->
+        GraphErrors graph
 
-def close() {
+        if (plotType == 'npheMean') {
+          graph = new GraphErrors(name)
+          graph.setTitle("Average HTCC Number of Photoelectrons per sector per ring")
+          graph.setTitleY("Average HTCC Number of Photoelectrons per sector per ring")
+          graph.setTitleX("run number")
+        } else if (plotType == 'normFactor') {
+          graph = new GraphErrors(name + " normalization factor")
+          graph.setTitle("Normalization factor (mean nphe per channel / average nphe across all channels) per sector per ring")
+          graph.setTitleY("Normalization factor per sector per ring")
+          graph.setTitleX("run number")
+        }
 
-  TDirectory out = new TDirectory()
-  out.mkdir('/timelines')
+        runs.sort { it.run }.each {
+          out.mkdir('/' + it.run)
+          out.cd('/' + it.run)
+          out.addDataSet(it.h1)
 
-  data.each{name,runs->
-    def grtl = new GraphErrors(name)
-    grtl.setTitle("Average HTCC Number of Photoelectrons per sector per ring")
-    grtl.setTitleY("Average HTCC Number of Photoelectrons per sector per ring")
-    grtl.setTitleX("run number")
+          if (plotType == 'npheMean') {
+            graph.addPoint(it.run, it.h1.getMean(), 0, 0)
+          } else if (plotType == 'normFactor') {
+            graph.addPoint(it.run, it.correctionFactor, 0, 0)
+          }
+        }
 
-    runs.sort{it.run}.each{
-      out.mkdir('/'+it.run)
-      out.cd('/'+it.run)
-      out.addDataSet(it.h1)
-      grtl.addPoint(it.run, it.h1.getMean(), 0, 0)
+        out.cd('/timelines')
+        out.addDataSet(graph)
+      }
+      out.writeFile("htcc_nphe_sec_ring_${plotType}.hipo")
     }
-    out.cd('/timelines')
-    out.addDataSet(grtl)
   }
 
-  out.writeFile('htcc_nphe_sec_ring.hipo')
-}
 }
--- detectors/src/main/java/org/jlab/clas/timeline/timeline/htcc/htcc_nphe_sector.groovy
+++ detectors/src/main/java/org/jlab/clas/timeline/timeline/htcc/htcc_nphe_sector.groovy
@@ -4,47 +4,65 @@ import org.jlab.groot.data.TDirectory
 import org.jlab.groot.data.GraphErrors
 
 class htcc_nphe_sector {
+  def data = new ConcurrentHashMap()
+  
+  def processDirectory(dir, run) {
+    // histogram with the nphe for all channels in a single run
+    def h_npheAll = dir.getObject('/HTCC/npheAll')
+    def averageNphe = h_npheAll.getMean() 
 
-def data = new ConcurrentHashMap()
+    (1..6).each{sec->
+      def hlist = [(1..4), [1,2]].combinations().collect{ring,side -> dir.getObject("/HTCC/H_HTCC_nphe_s${sec}_r${ring}_side$side")}
+      def h1 = hlist.head()
+      hlist.tail().each{h1.add(it)}
 
-def processDirectory(dir, run) {
-  (1..6).each{sec->
+      h1.setName("sec ${sec}")
+      h1.setTitle("HTCC Number of Photoelectrons")
+      h1.setTitleX("HTCC Number of Photoelectrons")
 
-    def hlist = [(1..4), [1,2]].combinations().collect{ring,side -> dir.getObject("/HTCC/H_HTCC_nphe_s${sec}_r${ring}_side$side")}
-    def h1 = hlist.head()
-    hlist.tail().each{h1.add(it)}
-
-    h1.setName("sec ${sec}")
-    h1.setTitle("HTCC Number of Photoelectrons")
-    h1.setTitleX("HTCC Number of Photoelectrons")
-
-    data.computeIfAbsent(sec, {[]}).add([run:run, h1:h1])
-  }
-}
-
-
-
-def close() {
-
-  TDirectory out = new TDirectory()
-  out.mkdir('/timelines')
-
-  data.each{sec,runs->
-    def grtl = new GraphErrors("sec$sec")
-    grtl.setTitle("Average HTCC Number of Photoelectrons per sector")
-    grtl.setTitleY("Average HTCC Number of Photoelectrons per sector")
-    grtl.setTitleX("run number")
-
-    runs.sort{it.run}.each{
-      out.mkdir('/'+it.run)
-      out.cd('/'+it.run)
-      out.addDataSet(it.h1)
-      grtl.addPoint(it.run, it.h1.getMean(), 0, 0)
+      def correctionFactor = averageNphe > 0 ? h1.getMean() / averageNphe : 0
+      // data.computeIfAbsent(sec, {[]}).add([run:run, h1:h1, correctionFactor:correctionFactor])
+      data.computeIfAbsent(sec, {[]}).add([run:run, h1:h1, mean:h1.getMean(), correctionFactor:correctionFactor])
     }
-    out.cd('/timelines')
-    out.addDataSet(grtl)
   }
 
-  out.writeFile('htcc_nphe_sec.hipo')
-}
+  def close() {
+    ['npheMean', 'normFactor'].each { plotType ->
+      TDirectory out = new TDirectory()
+      out.mkdir('/timelines')
+
+      data.each { sec, runs ->
+        GraphErrors graph
+
+        if (plotType == 'npheMean') {
+          graph = new GraphErrors("sec$sec")
+          graph.setTitle("Average HTCC Number of Photoelectrons per sector")
+          graph.setTitleY("Average HTCC Number of Photoelectrons per sector")
+          graph.setTitleX("run number")
+        } else if (plotType == 'normFactor') {
+          graph = new GraphErrors("sec$sec")
+          graph.setTitle("Normalization factor (mean nphe per channel / average nphe across all channels) per sector")
+          graph.setTitleY("Normalization factor per sector")
+          graph.setTitleX("run number")
+        }
+
+        runs.sort { it.run }.each {
+          out.mkdir('/' + it.run)
+          out.cd('/' + it.run)
+          out.addDataSet(it.h1)
+
+          if (plotType == 'npheMean') {
+            graph.addPoint(it.run, it.mean, 0, 0)
+          } else if (plotType == 'normFactor') {
+            graph.addPoint(it.run, it.correctionFactor, 0, 0)
+          }
+        }
+
+        out.cd('/timelines')
+        out.addDataSet(graph)
+      }
+      out.writeFile("htcc_nphe_sec_${plotType}.hipo")
+    }
+  }
+
 }

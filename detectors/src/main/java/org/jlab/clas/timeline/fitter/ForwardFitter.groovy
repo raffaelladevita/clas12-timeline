7befc45e0c226a714dc2ef8486a48e9aa09eaf8d
Mathieu Ouillon <67646911+mathieuouillon@users.noreply.github.com>
2025-04-08 12:02:43 -0400

    feat(timeline): add support for second peak data in forward tracking Vz (#297)

--- detectors/src/main/java/org/jlab/clas/timeline/fitter/ForwardFitter.groovy
+++ detectors/src/main/java/org/jlab/clas/timeline/fitter/ForwardFitter.groovy
@@ -11,20 +11,18 @@ import org.jlab.groot.math.F1D
 
 
 class ForwardFitter{
-static F1D fitBimodal(H1F h1) {
+static F1D fitBimodal(H1F h1, float mean1, float mean2, float sigma1, float sigma2, float range_min, float range_max) {
 	    def f1 = new F1D("fit:"+h1.getName(), "[amp1]*gaus(x,[mean1],[sigma1])+[amp2]*gaus(x,[mean2],[sigma2])+[p0]+[p1]*x", -9.0, 0.0);
         double hAmp  = h1.getBinContent(h1.getMaximumBin());
         double hMean = h1.getAxis().getBinCenter(h1.getMaximumBin());
         double hRMS  = h1.getRMS();
-        double rangeMin = (-9);
-        double rangeMax = (0);
-        f1.setRange(rangeMin, rangeMax);
+        f1.setRange(range_min, range_max);
         f1.setParameter(0, hAmp);
-        f1.setParameter(1, -8);
-        f1.setParameter(2, 0.5);
+        f1.setParameter(1, mean1);
+        f1.setParameter(2, sigma1);
 		f1.setParameter(3, hAmp);
-        f1.setParameter(4, -3);
-        f1.setParameter(5, 0.5);
+        f1.setParameter(4, mean2);
+        f1.setParameter(5, sigma2);
 		f1.setParameter(6, 1);
 		f1.setParameter(7, 1);
 		MoreFitter.fit(f1,h1,"LQ");
@@ -38,7 +36,7 @@ static F1D fitBimodal(H1F h1) {
 			hRMS1 = func.getParameter(2).abs()
 			hMean2 = func.getParameter(4)
 			hRMS2 = func.getParameter(5).abs()
-			func.setRange(-9, 0)
+			func.setRange(range_min, range_max)
 			MoreFitter.fit(func,h1,"Q")
 
 			return [func.getChiSquare(), (0..<func.getNPars()).collect{func.getParameter(it)}]
